a7b79f3de5229f4cd2068abaf14f58627e3b94f7
Dheeraj <dkjoshi8293@gmail.com>
2024-01-23 22:52:21 +0530

    Navbar and Signup page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,42 @@
+import Navbar from '@/components/Navbar'
+import { cn } from '@/lib/utils'
+import { Inter } from 'next/font/google'
+// import Providers from '@/components/Providers'
+import { Toaster } from '@/components/ui/Toaster'
 import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Opinify',
-  description: 'Wait for the description'
+  description: 'A Reddit clone built with Next.js and TypeScript.',
 }
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode
+  authModal: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang='en'
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}>
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        {/* <Providers> */}
+          <Navbar />
+          {/* {authModal} */}
+
+          <div className='container max-w-7xl mx-auto h-full pt-12'>
+            {children}
+          </div>
+        {/* </Providers> */}
+        <Toaster />
+      </body>
     </html>
   )
 }
---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { FC } from "react";
+import { Button } from "@/components/ui/Button";
+import { useToast } from "@/hooks/use-toast";
+import { Icons } from "./Icons";
+import { error } from "console";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      throw new Error();
+      await signIn("google");
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "There was an error logging in with Google",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={cn("flex justify-center", className)} {...props}>
+      <Button
+        isLoading={isLoading}
+        type="button"
+        size="sm"
+        className="w-full"
+        onClick={loginWithGoogle}
+        disabled={isLoading}
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
+        Google
+      </Button>
+    </div>
+  );
+};
+
+export default UserAuthForm;
