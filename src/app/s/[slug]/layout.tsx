1a0174e7d19fb1d62fd9d4008752c2fc6b59bdff
Dheeraj <dkjoshi8293@gmail.com>
2024-07-04 10:32:16 +0530

    Post editor checkpoint

--- src/app/s/[slug]/layout.tsx
+++ src/app/s/[slug]/layout.tsx
@@ -106,7 +106,7 @@ const Layout = async ({
                   variant: 'outline',
                   className: 'w-full mb-6',
                 })}
-                href={`${slug}/submit`}>
+                href={`s/${slug}/submit`}>
                 Create Post
               </Link>
             </dl>
